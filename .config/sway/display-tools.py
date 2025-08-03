2303c00f8ed32700769cef1318376c4c3a430712
q3aql <q3aql@duck.com>
2025-08-03 19:09:25 +0200

    .config/sway/display-tools.py

--- .config/sway/display-tools.py
+++ .config/sway/display-tools.py
@@ -17,7 +17,7 @@ class MainWindow:
 
     # Set theme and window size
     master.configure(bg='#23283f')
-    master.geometry('350x265')
+    master.geometry('350x315')
 
     self.boton1 = tk.Button(master, text="Run wdisplays", command=self.exec_command1, bg='#205d2c', fg="white")
     self.boton1.pack(side=tk.TOP, pady=10)
@@ -28,11 +28,14 @@ class MainWindow:
     self.boton3 = tk.Button(master, text="Run ~/.config/sway/wayland-screenshot", command=self.exec_command3, bg='#205d2c', fg="white")
     self.boton3.pack(side=tk.TOP, pady=10)
 
-    self.boton4 = tk.Button(master, text="Start/Restart nwg-wrapper (conky sway)", command=self.exec_command4, bg='#205d2c', fg="white")
+    self.boton4 = tk.Button(master, text="Run ~/.config/sway/wayland-recorder", command=self.exec_command4, bg='#205d2c', fg="white")
     self.boton4.pack(side=tk.TOP, pady=10)
-    
-    self.boton5 = tk.Button(master, text="Exit", command=self.exec_command5, bg='#205d2c', fg="white")
+
+    self.boton5 = tk.Button(master, text="Start/Restart nwg-wrapper (conky sway)", command=self.exec_command5, bg='#205d2c', fg="white")
     self.boton5.pack(side=tk.TOP, pady=10)
+    
+    self.boton6 = tk.Button(master, text="Exit", command=self.exec_command6, bg='#205d2c', fg="white")
+    self.boton6.pack(side=tk.TOP, pady=10)
 
     # Set center buttons
     self.boton1.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH)
@@ -40,6 +43,7 @@ class MainWindow:
     self.boton3.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH)
     self.boton4.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH)
     self.boton5.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH)
+    self.boton6.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH)
 
   def exec_command1(self):
     print('# Run wdisplays')
@@ -57,11 +61,16 @@ class MainWindow:
     os.system(command)
 
   def exec_command4(self):
+    print('# Run ~/.config/sway/wayland-recorder')
+    command = "~/.config/sway/wayland-recorder"
+    os.system(command)
+
+  def exec_command5(self):
     print('# Start/Restart nwg-wrapper (conky sway)')
     command = "~/.config/sway/nwg-wrapper-conky.sh"
     os.system(command)
 
-  def exec_command5(self):
+  def exec_command6(self):
     command = "exit"
     os.system(command)
     exit()
