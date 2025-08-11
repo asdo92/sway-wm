41fc2c84c7120f51477db10b2db39031b3be4376
q3aql <q3aql@duck.com>
2025-08-11 14:54:40 +0200

    .config/waybar/scripts

--- .config/waybar/scripts/kernel_version.sh
+++ .config/waybar/scripts/kernel_version.sh
@@ -30,5 +30,5 @@ kernelVersion=${kernelVersion}${kernelVersionTempDot}
 #kernelVersion=${kernelVersion}-${archCommand}
 kernelVersion=${kernelVersion}
 #echo "Kernel: Linux ${kernelVersion} "
-echo " ${kernelVersion} "
+echo "󰌽 ${kernelVersion} "
 
