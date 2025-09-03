6d1f462be35ba33191b1ceafb98d2c838dfe4c50
q3aql <q3aql@duck.com>
2025-09-03 23:09:31 +0200

    .config/waybar/scripts/get_volume.sh

--- .config/waybar/scripts/get_volume.sh
+++ .config/waybar/scripts/get_volume.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-get_volume=$(amixer | grep "%" | head -1 | cut -d "%" -f 1 | cut -d "[" -f 2)
-echo " ${get_volume}% "
+#get_volume=$(amixer | grep "%" | head -1 | cut -d "%" -f 1 | cut -d "[" -f 2)
+get_volume=$(pactl list sinks | grep "Volume:" | head -1 | awk '{print $5}')
+echo " ${get_volume} "
