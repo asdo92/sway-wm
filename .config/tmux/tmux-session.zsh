60f9aaa782c4b5fa8df9fe3c57b753bf50c0cbdd
dmesg00 <dmesg00@duck.com>
2026-08-30 22:07:57 +0200

    .config/tmux

--- .config/tmux/tmux-session.zsh
+++ .config/tmux/tmux-session.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 
 # Set windows session
-tmux new-session -d -s work -n terminal1
-tmux new-window -t work:1 -n terminal2
-tmux new-window -t work:2 -n terminal3
-tmux new-window -t work:3 -n terminal4
-tmux new-window -t work:4 -n terminal5
+tmux new-session -d -s work -n "#{client_tty}"
+tmux new-window -t work:1 -n "#{client_tty}"
+tmux new-window -t work:2 -n "#{client_tty}"
+tmux new-window -t work:3 -n "#{client_tty}"
+tmux new-window -t work:4 -n "#{client_tty}"
 tmux new-window -t work:5 -n ssmtunel
-tmux new-window -t work:6 -n lan-100
-tmux new-window -t work:7 -n lan-50
+tmux new-window -t work:6 -n lan-100 \
+  'echo lan100 ; /usr/bin/zsh'
+tmux new-window -t work:7 -n lan-50 \
+  'echo lan50 ; /usr/bin/zsh'
 tmux attach-session -t work
